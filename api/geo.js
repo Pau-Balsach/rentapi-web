8fb824929c1414381f36c6c1bcfc91463ea2731a
pbalsach <sigmasogma335@gmail.com>
2026-06-02 07:44:28 +0200

    GEO UPDATE

--- api/geo.js
+++ api/geo.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // La ruta puede venir como ?path=ciudades o capturada por el rewrite /(.*) 
   const { path, '0': captured, ...query } = req.query
   const subpath = path || captured || req.url.replace(/^\/api\/geo\/?/, '').split('?')[0]
 
@@ -14,6 +13,7 @@ export default async function handler(req, res) {
       }
     })
     const data = await response.json()
+    res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate=3600')
     res.status(response.status).json(data)
   } catch (error) {
     res.status(500).json({ error: 'Error conectando con RentAPI', detail: error.message })
